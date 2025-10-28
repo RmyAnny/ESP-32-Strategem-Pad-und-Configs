a3cea6a09bd3d4b3f945af1613f364dc8f60b143
RmyAnny <158101268+RmyAnny@users.noreply.github.com>
2025-10-28 23:00:35 +0100

    Create User_Setup_Demo_GUI.h
    gehört zur Demo_GUI_Pad.cpp !!!!!

---
+++ User_Setup_Demo_GUI.h
@@ -0,0 +1,27 @@
+//WICHTIG diesen Code in die User_Setup.h kopieren bzw. Datei genau  User_Setup.h nennen
+//das sind die Configs für die Demo_GUI_Pad.cpp 
+
+#define USER_SETUP_ID 2432
+#define ST7789_DRIVER
+#define TFT_WIDTH  320
+#define TFT_HEIGHT 480
+
+#define TFT_MOSI 13
+#define TFT_SCLK 14
+#define TFT_CS   15
+#define TFT_DC   2
+#define TFT_RST  12
+#define TFT_BL   27
+
+#define TFT_RGB_ORDER TFT_BGR
+#define TFT_INVERSION_OFF
+
+#define LOAD_GLCD
+#define LOAD_FONT2
+#define LOAD_FONT4
+#define LOAD_FONT6
+#define LOAD_FONT7
+#define LOAD_FONT8
+#define LOAD_GFXFF
+#define SMOOTH_FONT
+#define SPI_FREQUENCY 27000000
